b6cadd6267926f388074130da53ed2e381274110
barnez299 <barnez29@gmail.com>
2021-01-19 01:41:08 +0200

    requests object working

---
+++ flaskyoutubeapi/routes.py
@@ -0,0 +1,24 @@
+import requests
+from flask import Blueprint, render_template, current_app
+
+
+main = Blueprint('main', __name__)
+
+@main.route("/")
+def index():
+    search_url = 'https://www.googleapis.com/youtube/v3/search'
+    search_params = {
+        'key': current_app.config['YOUTUBE_API_KEY'],
+        'q': 'learn flask',
+        'part': 'snippet',
+        'max results': 9,
+        'type': 'video'
+
+    }
+
+    r = requests.get(search_url, params=search_params)
+
+    print(r.json()['items'][0]['id']['videoId'])
+
+    return render_template('index.html')
+
