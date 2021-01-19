8edf22dcdd81a3a6e9a86a70be46590e5e8cfcce
barnez299 <barnez29@gmail.com>
2021-01-19 03:07:21 +0200

    video routes working

--- flaskyoutubeapi/routes.py
+++ flaskyoutubeapi/routes.py
@@ -7,6 +7,10 @@ main = Blueprint('main', __name__)
 @main.route("/")
 def index():
     search_url = 'https://www.googleapis.com/youtube/v3/search'
+    video_url = 'https://www.googleapis.com/youtube/v3/videos'
+
+
+
     search_params = {
         'key': current_app.config['YOUTUBE_API_KEY'],
         'q': 'learn flask',
@@ -26,7 +30,37 @@ def index():
     for result in results:
         video_ids.append(result['id']['videoId'])
 
-    print(video_ids)
 
-    return render_template('index.html')
+
+    video_params = {
+        'key': current_app.config['YOUTUBE_API_KEY'],
+        'id': ','.join(video_ids),
+        'part': 'snippet,contentDetails',
+        'maxResults': 9,
+
+    }
+
+
+    
+    r = requests.get(video_url, params=video_params)
+    results = r.json()['items']
+    
+    videos =[]
+    for result in results:
+        video_data = {
+            'id':result['id'],
+            'url': f'https://www.youtube.com/watch?v={ result["id"] }',
+            'thumbnail': result['snippet']['thumbnails']['high']['url'],
+            'duration': result['contentDetails']['duration'],
+            'title': result['snippet']['title']
+        }
+        videos.append(video_data)
+
+        # print(video_data)
+
+
+
+
+
+    return render_template('index.html', videos=videos)
 
